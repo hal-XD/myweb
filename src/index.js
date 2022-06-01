f976f88858ebaf3190998dd1cb33efdc4b05cf98
k8s-1 <k8s@example.com>
2022-06-01 22:39:25 +0900

    baisic routing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
-
-function MyComp(props) {
-  return <h1>my comp {props.name}</h1>
-}
+import { BrowserRouter,Link,Routes,Route } from 'react-router-dom';
 
 class Header extends React.Component {
   render() {
@@ -17,120 +14,54 @@ class Header extends React.Component {
   }
 }
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props); // 常に呼び出す。
-    this.state = {date:new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),1000
-    )
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID)
-  }
-
-  tick() {
-    this.setState({
-        date: new Date(),
-    });
-  }
-
-  render () {
+class Nav extends React.Component {
+  render() {
     return (
-      <div>
-        <h1>Clock Component</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-      </div>
+      <>
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+        }}>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/invoices">Invoices</Link></li>
+            <li><Link to="/expenses">Expenses</Link></li>
+          </ul>
+        </nav>
+      </>
     )
   }
 }
 
-function Form() {
+function Expenses() {
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      console.log('Clicked');
-    }}>
-      <button type="submit">submit!!</button>
-    </form>
+    <main style={{ padding: "1rem 0"}}>
+      <h2>Expenses</h2>
+    </main>
   )
-}
-
-function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
-  );
-}
-
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
-
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick(){
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
-  }
-  render(){
-    return(
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn?'ON':'OFF'}
-      </button>
-    )
-  }
-}
-
-function Greeting(props) {
-  const isLoggdIn = props.isLoggdIn;
-  if (isLoggdIn) {
-    return <UserGreeting />
-  }
-  return <GuestGreeting />
-}
-
-function UserGreeting(props) {
-    return (
-      <h1>hello, user1</h1>
-    )
-}
-
-function GuestGreeting(props) {
-    return (
-      <h1>hello, Guest</h1>
-    )
 }
 
 class Base extends React.Component {
   render() {
     return (
-        <div>
-          <Header />
-          <h1>hello,component {this.props.name}</h1>
-          <MyComp name="emily"></MyComp>
-          <Form />
-          <Toggle />
-          <Greeting isLoggdIn={true} />
-          <Clock />
-        </div>
+        <>
+          <h1>Base</h1>
+        </>
       );
   }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Base name="my component"/>);
+root.render(
+          <>
+          <BrowserRouter>
+            <Header />
+            <Nav></Nav>
+            <Routes>
+              <Route path="/" element={<Base />}/>
+              <Route path="/expenses" element={<Expenses />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+);
