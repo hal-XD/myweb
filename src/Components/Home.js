7b691266c4e64474e7d0da91015ba4205867154a
k8s-1 <k8s@example.com>
2022-06-03 01:52:41 +0900

    hook

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 import "../index.css"
 
 export default function Home() {
+    const [cjsx, setJsx] = useState(0);
     return(
-        <div className="content">Home</div>
+        <div className="content">
+            <h1>Home</h1>
+            <button onClick={() => {
+                setJsx(cjsx+1)
+            }}>Add a Content.</button>
+            <h1>cout: {cjsx}</h1>
+        </div>
     )
 }
