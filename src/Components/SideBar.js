c7464b9c3a4cd94aff52d38f3f7f7926af2396c0
k8s-1 <k8s@example.com>
2022-06-04 00:45:07 +0900

    creating sidbar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom'
 
 export default function SideBar() {
     return (
-        <div className="bg-red-600 w-2/12" >
-            <p>side test</p>
-            <ul class="text-black font-extrabold text-center">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/game">Game</Link></li>
-                <li><Link to="/todo">ToDo</Link></li>
-            </ul>
+        <div className="bg-blue-600 w-2/12" >
+         <div className='fixed h-3/6 '>
+          <ul className="text-black font-extrabold text-center -top-1/2">
+           <li className="hover:bg-blue-300"><Link to="/">Home</Link></li>
+           <li className="hover:bg-blue-300"><Link to="/game">Game</Link></li>
+           <li className="hover:bg-blue-300"><Link to="/todo">ToDo</Link></li>
+          </ul>
+         </div>
         </div>
     )
 }
