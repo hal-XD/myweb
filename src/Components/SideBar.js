8789d7309f5bc0cfb8e7866a15fb39c18df444e7
k8s-1 <k8s@example.com>
2022-06-04 22:35:43 +0900

    adjust sidebar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 export default function SideBar() {
     return (
         <div className="bg-blue-600 w-2/12" >
-         <div className='fixed h-3/6 '>
-          <ul className="text-black font-extrabold text-center -top-1/2">
-           <li className="hover:bg-blue-300"><Link to="/">Home</Link></li>
-           <li className="hover:bg-blue-300"><Link to="/game">Game</Link></li>
-           <li className="hover:bg-blue-300"><Link to="/todo">ToDo</Link></li>
+         <div className='fixed h-3/6 w-2/12'>
+          <ul className="text-black font-extrabold text-center">
+           <li className="hover:bg-blue-300 m-2"><Link to="/">Home</Link></li>
+           <li className="hover:bg-blue-300 m-2"><Link to="/game">Game</Link></li>
+           <li className="hover:bg-blue-300 m-2"><Link to="/todo">ToDo</Link></li>
           </ul>
          </div>
         </div>
